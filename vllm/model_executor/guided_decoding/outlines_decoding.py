3254489eaf2a26e4ed5bb30bf7688ed480b66a71
Paul Grundmann <paul.grundmann@snafu.de>
2024-08-07 09:43:12 +0200

    reduce num workers to 1

--- vllm/model_executor/guided_decoding/outlines_decoding.py
+++ vllm/model_executor/guided_decoding/outlines_decoding.py
@@ -71,7 +71,7 @@ async def get_outlines_guided_decoding_logits_processor(
 
     if global_thread_pool is None:
         global_thread_pool = concurrent.futures.ThreadPoolExecutor(
-            max_workers=2)
+            max_workers=1)
     loop = asyncio.get_running_loop()
 
     return await loop.run_in_executor(global_thread_pool,
